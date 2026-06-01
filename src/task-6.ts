bab740da1fb51e3e3d6daf1ee003926196bc0c62
akrep17m-png <akr.ep17m@gmail.com>
2026-06-01 12:06:01 +0300

    fix: add explicit generic types to function calls in getFirstElement

--- src/task-6.ts
+++ src/task-6.ts
@@ -7,10 +7,10 @@ function getFirstElement<T>(arr: T[]): T {
   return arr[0];
 }
 
-getFirstElement([1, 2, 3]); // 1
-getFirstElement(["a", "b", "c"]); // "a"
-getFirstElement([true, false, true]); // true
+getFirstElement<number>([1, 2, 3]); // 1
+getFirstElement<string>(["a", "b", "c"]); // "a"
+getFirstElement<boolean>([true, false, true]); // true
 
-console.log(getFirstElement([1, 2, 3])); // 1
-console.log(getFirstElement(["a", "b", "c"])); // "a"
-console.log(getFirstElement([true, false, true])); // true
+console.log(getFirstElement<number>([1, 2, 3])); // 1
+console.log(getFirstElement<string>(["a", "b", "c"])); // "a"
+console.log(getFirstElement<boolean>([true, false, true])); // true
